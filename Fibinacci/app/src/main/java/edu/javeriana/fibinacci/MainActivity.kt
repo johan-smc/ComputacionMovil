105a9c86b0c3f3b21bf785c508b1178d03e1fc92
Juan Pablo Peñaloza Botero <juan.pa097@gmail.com>
2019-08-10 18:25:00 -0500

    Finished first part of the workshop

--- Fibinacci/app/src/main/java/edu/javeriana/fibinacci/MainActivity.kt
+++ Fibinacci/app/src/main/java/edu/javeriana/fibinacci/MainActivity.kt
@@ -2,11 +2,23 @@ package edu.javeriana.fibinacci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var fibonacci: IntArray = intArrayOf(0, 1)
+    // private val FIBONACCI_IMG_URL = "https://cdn.images.express.co.uk/img/dynamic/109/590x/fibonaci.jpg"
+    private lateinit var sequenceTextView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.sequenceTextView = findViewById(R.id.text_sequence)
+        this.populateSequence()
+    }
+
+    private fun populateSequence() {
+        this.sequenceTextView.text = fibonacci.joinToString()
     }
 }
