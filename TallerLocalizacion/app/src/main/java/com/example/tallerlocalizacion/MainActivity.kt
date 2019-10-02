48ae4ff7105e0127144e9a17b940e5a726529d59
Juan Pablo Penaloza <jpenaloza@endava.com>
2019-10-01 19:47:05 -0500

    finished contact lists

--- TallerLocalizacion/app/src/main/java/com/example/tallerlocalizacion/MainActivity.kt
+++ TallerLocalizacion/app/src/main/java/com/example/tallerlocalizacion/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.example.tallerlocalizacion
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupButtons()
+    }
+
+    fun setupButtons() {
+        contactsButton.setOnClickListener { goToContactListActivity() }
+    }
+
+    fun goToContactListActivity() {
+        val goToContactListIntent = Intent(this, ContactList::class.java)
+        startActivity(goToContactListIntent)
     }
 }
